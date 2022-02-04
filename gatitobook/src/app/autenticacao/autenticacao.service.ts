d903c6229f98b040ae527fd09a1014f1766fd8e5
Murilo Ribeiro <88835239+MuriloRibg@users.noreply.github.com>
2022-02-04 00:27:09 -0300

    Atualizando a main

--- gatitobook/src/app/autenticacao/autenticacao.service.ts
+++ gatitobook/src/app/autenticacao/autenticacao.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { UsuarioService } from './usuario/usuario.service';
 import { tap } from 'rxjs/operators';
+
+import { UsuarioService } from './usuario/usuario.service';
+import { environment } from './../../environments/environment';
+
+const API = environment.apiURL;
 
 @Injectable({
   providedIn: 'root',
@@ -16,7 +20,7 @@ export class AutenticacaoService {
   autenticar(usuario: string, senha: string): Observable<HttpResponse<any>> {
     return this.httpClient
       .post(
-        'http://localhost:3000/user/login',
+        `${API}/login`,
         {
           userName: usuario,
           password: senha,
